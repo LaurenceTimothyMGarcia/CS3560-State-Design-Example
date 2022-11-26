3dfb62fb7351dd14e7e450be776033d9b59e6086
Laurence Timothy Garcia <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2022-11-26 12:51:18 -0800

    finished state machine

--- AudioPlayer.java
+++ AudioPlayer.java
@@ -3,6 +3,79 @@
 public class AudioPlayer
 {
 
+    private State state;
+
+    private UserInterface UI;
+    private Volumne vol;
+    private ArrayList playlist;
+    private Song currentSong;
+
+    public AudioPlayer()
+    {
+        state = new ReadyState(this);
+
+        UI = new UserInterface();
+        UI.lockButton.onClick(this.clickLock)
+        UI.playButton.onClick(this.clickPlay)
+        UI.nextButton.onClick(this.clickNext)
+        UI.prevButton.onClick(this.clickPrevious)
+    }
+
+    public void changeState(State state)
+    {
+        this.state = state;
+    }
+
+    public void clickLock()
+    {
+        state.clickLock();
+    }
+
+    public void clickPlay()
+    {
+        state.clickPlay();
+    }
+
+    public void clickNext()
+    {
+        state.clickNext();
+    }
+
+    public void clickPrevious()
+    {
+        state.clickPrevious();
+    }
+
+    public void startPlayback()
+    {
+        //Play music
+    }
+
+    public void stopPlayback()
+    {
+        //Stop playing music
+    }
+
+    public void nextSong()
+    {
+        //Skip to next song
+    }
+
+    public void previousSong()
+    {
+        //Go to previous song
+    }
+
+    public void fastForward(float time)
+    {
+        //Move forward x amount of seconds
+    }
+
+    public void rewind(float time)
+    {
+        //Move backwards x amoutn of seconds
+    }
+
 }
 
 abstract class State
